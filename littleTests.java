f9238e32230e5a4de7a0cfdaba52e88a69abef5a
lizardzhou <32279388+lizardzhou@users.noreply.github.com>
2017-09-28 14:36:54 +0200

    Update littleTests.java
    Delete package name and unused commands. Add explanations to methods.

--- littleTests.java
+++ littleTests.java
@@ -1,11 +1,9 @@
-package stupid;
 
-//import java.util.*;
-
-//A class for little tests.
+//A class for outputting all prime numbers in a given range.
 
 public class littleTests {
 	
+	//calculate the number of divisors of a given number
 	public static int noOfDivisors(int n) {
 		int count = 0;
 		for(int divisor = 2; divisor <= n-1; divisor++) {
@@ -16,6 +14,7 @@ public class littleTests {
 		return count;
 	}
 	
+	//if a number n does not have divisors in interval [2, n], it is a prime number
 	public static void primeNumsTo(int n) {
 		for(int i = 2; i<=n; i++) {
 			if(noOfDivisors(i) == 0) {
